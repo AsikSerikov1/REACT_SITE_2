78fd6c5af26e245131ac90b5638cd3369889ecb1
Asik <linzeb22@gmail.com>
2024-11-30 03:32:08 +0500

    default props

--- src/components/sw-components/person-details.js
+++ src/components/sw-components/person-details.js
@@ -10,6 +10,7 @@ const PersonDetails = (props) => {
             <Record field="eyeColor" label="Eye Color" />
             <Record field="height" label="Height" />
             <Record field="birthYear" label="Birth Year" />
+            <Record field="homeworld" label="Homeworld" />
         </ItemDetails>
     )
 }
@@ -21,4 +22,4 @@ const mapMethodsToProps = (swapiService) => {
     }
 }
 
-export default withSwapiService(PersonDetails, mapMethodsToProps)
+export default withSwapiService(mapMethodsToProps)(PersonDetails)
