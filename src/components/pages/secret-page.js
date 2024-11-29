a79f4313f9bd81374b44370789030709e7b85083
Asik <linzeb22@gmail.com>
2024-11-30 03:54:48 +0500

    login, secret pages; select paths, 404 handler

---
+++ src/components/pages/secret-page.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Redirect } from "react-router-dom"
+const SecretPage = ({ isLoggedIn }) => {
+    if (isLoggedIn) {
+        return (
+            <div className="jumbotron text-center">
+                <h4>This page is full of secrets!!!</h4>
+            </div>
+        )
+    }
+    return <Redirect to="/login" />
+}
+export default SecretPage
